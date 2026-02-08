723d7bc63930f203e850d0865b8c029f220b9516
Lilie <firlyyramadhanit@gmail.com>
2026-02-08 16:54:50 +0700

    cari dan tambah barang

--- Pertemuan2/tugas1.py
+++ Pertemuan2/tugas1.py
@@ -34,7 +34,7 @@ def baca_stok(nama_file):
 #------------------
 #fungsi 2: menampilkan semua data
 #------------------
-def tampilkan_semua(stok_dict):
+def tampilkan_semua(stock_dict):
     """Menampilkan semua barang yang ada di stok_dict"""
     #membuat header tabel
     print("\n=== DAFTAR STOK BARANG LISHOP ===")
@@ -46,12 +46,56 @@ def tampilkan_semua(stok_dict):
             print(f"| {"Kode Barang" : <12} | {"Nama Barang" : <12} | {"Stok" : >5} |")
             print("-" * 39)
             #menampilkan isi data stok barang
-            for KodeBarang in sorted(stok_dict.keys()):
-                NamaBarang = stok_dict[KodeBarang]["Nama Barang"]
-                Stok = stok_dict[KodeBarang]["Stok Barang"]
+            for KodeBarang in sorted(stock_dict.keys()):
+                NamaBarang = stock_dict[KodeBarang]["Nama Barang"]
+                Stok = stock_dict[KodeBarang]["Stok Barang"]
                 print(F"| {KodeBarang:<12} | {NamaBarang:<12} | {int(Stok):>5} |")
             print("-" *39)
 
-#buka_data = baca_stok(nama_file)
+buka_data = baca_stok(nama_file)
 #tampilkan_semua(buka_data) #menampilkan data
 
+#------------------
+#fungsi 3: mencari stok barang berdasarkan kode barang
+#------------------
+def cari_barang(stock_dict):
+    """Mencari barang berdasarkan kode barang"""
+    #membuat input kode barang yang ingin dicari
+    kode_cari = input("Masukkan kode barang yang ingin anda cari: ").strip()
+
+    if kode_cari in stock_dict: #mengecek apakah kode ada di dalam data barang
+        Nama = stock_dict[kode_cari]["Nama Barang"]
+        Stok = stock_dict[kode_cari]["Stok Barang"]
+        print("===Data Barang Ditemukan!===")
+        print(F"Kode Barang: {kode_cari}")
+        print(F"Nama Barang: {Nama}")
+        print(f"Stok Barang: {Stok}")
+    else:
+        print("Barang yang anda cari tidak ditemukan! Pastikan kode barang benar!")
+
+#cari_barang(buka_data)
+
+#------------------
+#fungsi 4: menambah barang baru
+#------------------
+def tambah_barang(stok_dict):
+    """Menambah barang baru ke dalam stock_dict"""
+    print("===Menambah Barang Baru===")
+
+    while True:
+        kode = input("Masukkan kode barang baru: ").strip()
+        if kode in stok_dict:
+            print("Maaf kode barang sudah dipakai! Silahkan gunakan kode lain")
+        else:
+            break
+
+    nama = input("Masukkan nama barang: ").strip()
+    stok_awal = int(input("Masukkan jumlah stok awal: "))
+    stok_dict[kode]= {
+        "Nama Barang" : nama,
+        "Stok Barang" : int(stok_awal)
+    }
+    print("Barang berhasil ditambahkan!")
+
+
+#tambah_barang(buka_data)
